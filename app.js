81390bcac31f4ce06e1fac7ce24a08e6b03de83f
nicozapatacruz <nicozapatacruz@gmail.com>
2021-11-03 21:04:39 -0500

    Cambie el nombre de Cart y Detail (html) y la ubicacion de el archivo de diseno-grafico

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const express = require("express")
-const app = express()
-
-const path = require("path")
-
-const publicDomain = path.resolve(__dirname, "./public")
-
-app.use(express.static(PublicDomain))
+const express = require("express");
+const app = express();
+const path = require("path");
 
 app.listen(4000, () => {
-    console.log("Servidor corriendo en el puerto: 4000")
-})
+  console.log("Servidor corriendo en el puerto: 4000");
+});
 
-app.get("/", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "/views/index.html"))
-})
+const publicDomain = path.resolve(__dirname, "./public");
+app.use(express.static(publicDomain));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "/views/index.html"));
+});
