e35141249e157c128d66829abe50e14279bc8c5b
milto <miltondemarte@gmail.com>
2021-11-23 23:47:13 -0300

    implementando principcios de CRUD

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,8 +1,12 @@
-const path = require("path")
+const fs = require("fs")
+const productsJSON = fs.readFileSync("src/database/productos.json" , "utf-8")
+const products = JSON.parse(productsJSON)
 
 const mainController = {
     index: (req,res) =>{
-        res.render("../src/views/index")
+        res.render("../src/views/index",{
+            products,
+        })
     },
 
 }
