ba678140d0d6b5203cf045215cbebfbdc4fc4d82
nicozapatacruz <nicozapatacruz@gmail.com>
2022-01-04 17:09:25 -0500

    Correccion Validations y Re Disennio pagina Login

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,9 +1,17 @@
-const genders = ["RPG", "Action", "Adventure", "Sports", "Shooters", "Indie", "Race", "Puzzles"]
+const genders = [
+  "RPG",
+  "Action",
+  "Adventure",
+  "Sports",
+  "Shooters",
+  "Indie",
+  "Race",
+  "Puzzles",
+];
 
-const gamesServices = require("../services/gamesServices")
-const consolServices = require("../services/consolServices")
-const {validationResult} = require("express-validator")
-
+const gamesServices = require("../services/gamesServices");
+const consolServices = require("../services/consolServices");
+const { validationResult } = require("express-validator");
 
 const gamesController = {
   /*INDEX*/
@@ -11,57 +19,56 @@ const gamesController = {
     res.render("games/details", {
       consols: consolServices.getAll(),
       games: gamesServices.getAll(),
-    })
+    });
   },
 
   /**GAME SERVICES**/
   /*Create Game Form*/
   createGame: (req, res) => {
-    res.render("games/gamesCreation",{
-      genders
-    })
+    res.render("games/gamesCreation", {
+      genders,
+    });
   },
 
   /*Save Game*/
   storeGame: (req, res) => {
-    const errors = validationResult(req)
+    const errors = validationResult(req);
 
-    if(errors.isEmpty()){
-      gamesServices.create(req.body)
-      res.redirect("/games")
+    if (errors.isEmpty()) {
+      gamesServices.create(req.body);
+      res.redirect("/games");
     } else {
-      res.render("games/gamesCreation",{
-        errors : errors.array(),
-        old : req.body,
-        genders
-      })
+      res.render("games/gamesCreation", {
+        errors: errors.mapped(),
+        old: req.body,
+        genders,
+      });
     }
-    
   },
 
   /*Edit Game*/
   editGame: (req, res) => {
-    idSearch = req.params.id
-    const game = gamesServices.findOne(idSearch)
+    idSearch = req.params.id;
+    const game = gamesServices.findOne(idSearch);
 
     res.render("games/gamesEdit", {
       game,
-    })
+    });
   },
 
   /*Update Game*/
   updateGame: (req, res) => {
-    const idSearch = req.params.id
-    gamesServices.update(idSearch, req.body)
-    res.redirect("/games")
+    const idSearch = req.params.id;
+    gamesServices.update(idSearch, req.body);
+    res.redirect("/games");
   },
 
   /*Destroy Game*/
   destroyGame: (req, res) => {
-    const idSearch = req.params.id
-    gamesServices.destroy(idSearch)
-    res.redirect("/games")
-  }
-}
+    const idSearch = req.params.id;
+    gamesServices.destroy(idSearch);
+    res.redirect("/games");
+  },
+};
 
-module.exports = gamesController
+module.exports = gamesController;
--- src/validations/validationsUsers.js
+++ src/validations/validationsUsers.js
@@ -20,7 +20,25 @@ module.exports = [
     .withMessage("Ingresar una direccion de envio valida")
     .bail(),
 
-  //   check("image").notEmpty().withMessage("Seleccionar una imagen de usuario").bail(),
+  check("image").custom((value, { req }) => {
+    let file = req.file;
+    let acceptedExtensions = [".jpg", ".png", ".gif"];
+
+    if (!file) {
+      throw new Error("Tienes que subir una imagen");
+    } else {
+      let fileExtension = path.extname(file.originalname);
+      if (!acceptedExtensions.includes(fileExtension)) {
+        throw new Error(
+          `Las extensiones de archivo permitidas son ${acceptedExtensions.join(
+            ", "
+          )}`
+        );
+      }
+    }
+
+    return true;
+  }),
 
   check("password")
     .notEmpty()
