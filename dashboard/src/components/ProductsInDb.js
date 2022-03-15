bb8238927c9027b2ee5e3b9d5afef03500507181
nicozapatacruz <nicozapatacruz@gmail.com>
2022-03-15 17:24:12 -0400

    Images in React

--- dashboard/src/components/ProductsInDb.js
+++ dashboard/src/components/ProductsInDb.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { API_HOST } from "../constants";
-import imagenFondo from "../assets/images/1646103572318_product.JPG";
+// import imagenFondo1 from "../assets/images/1646103572318_product.JPG";
+// import imagenFondo2 from "../assets/images/1646103572318_product.JPG";
+// import imagenFondo3 from "../assets/images/1646103572318_product.JPG";
 
 function ProductsInDb() {
   const [productList, setProductList] = useState([]);
@@ -21,6 +23,9 @@ function ProductsInDb() {
   return (
     <div className="row">
       {productList.map((product, i) => {
+        console.log(product);
+        let productImage = `../../../website/public/images/productos/${product.product_image}`;
+        console.log(productImage);
         return (
           <div className="col-lg-6 mb-4">
             <div className="card shadow mb-4">
@@ -29,7 +34,7 @@ function ProductsInDb() {
               </div>
               <div key={i} className="card-body">
                 <div className="text-center">
-                  <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: 40 + "rem" }} src={imagenFondo} alt="imagen producto" />
+                  <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: 40 + "rem" }} src={process.env.PUBLIC_URL + `/img/${product.product_image}`} alt="imagen producto" />
                   <p>{product.price}</p>
                   <p>{product.description}</p>
                 </div>
